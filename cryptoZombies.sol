27addae153e67cafe73d8eabbb17d9e17e68617d
WillLewis <willxemail@gmail.com>
2022-04-08 18:43:13 -0500

    chapter 3 assigned ownership of zombie to whoever calls function

--- cryptoZombies.sol
+++ cryptoZombies.sol
@@ -18,8 +18,9 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) -1;
-        //event fired here
-        emit NewZombie(id, _name, _dna);
+        zombieToOwner[msg.sender] = id; //assign ownership of zombie to whoever calls function
+        ownerZombieCount[msg.sender] = uint++; //increment their owned zombie count
+        emit NewZombie(id, _name, _dna);//event fired here
     } 
     
      
