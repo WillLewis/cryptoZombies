c185697ba8f3c085ccaf9ce94f778687b3f11260
WillLewis <willxemail@gmail.com>
2022-04-14 17:14:31 -0500

    2.13 - add special features to kitty dna using an if statement to check if species is kitty

--- zombieFeeding.sol
+++ zombieFeeding.sol
@@ -22,18 +22,23 @@ contract ZombieFeeding is ZombieFactory { //demonstrating inheritance
   	KittyInterface kittyContract = KittyInterface(ckAddress); //now kittyContract is point to the cryptokitty contract
 
 
-	function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+	function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
 		require(msg.sender == zombieToOwner[_zombieId]); //make sure owner owns this zombie
 		Zombie storage myZombie = zombies[_zombieId]; //create a pointer to index of owners zombie
 		_targetDna = _targetDna % dnaModulus; //to only take last 16 digits
-		uint newDna = (myZombie.dna + _targetDna) / 2;
+		uint newDna = (myZombie.dna + _targetDna) / 2; //calculate new zombies DNA
+		if(keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+			newDna = newDna - newDna % 100 + 99;
+			//adding 99 on last 2 digits of DNA to signify they are cats (bc cats have 9 lives)
+			//Assume newDna is 334455. Then newDna % 100 is 55, so newDna - newDna % 100 is 334400. Finally add 99 to get 334499
+		}
 		_createZombie("NoName", newDna);
 	}
 
 	function feedOnKitty(uint _zombieId, uint _kittyId) public { //gets kitty genes from contract
 		uint kittyDna;
 		(,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId); //receiving tenth item in getKitty which returns 10 items
-		feedAndMultiply(_zombieId, kittyDna);
+		feedAndMultiply(_zombieId, kittyDna, "kitty");
 	}
 	
 }
