c26c3740dc878a47ae4bb6ae1f4a606c3f363cb6
WillLewis <willxemail@gmail.com>
2022-04-14 15:23:43 -0500

    2.7 - add new function. require ownership of the zombie to activate function. create a pointer to the zombie in the index

--- zombieFeeding.sol
+++ zombieFeeding.sol
@@ -4,4 +4,10 @@ import "./zombieFactory.sol";
 
 contract ZombieFeeding is ZombieFactory { //demonstrating inheritance
 
+	function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+		require(msg.sender == zombieToOwner[_zombieId]); //make sure owner owns this zombie
+		Zombie storage myZombie = zombies[_zombieId]; //create a pointer to index of owners zombie
+
+	}
+
 }
