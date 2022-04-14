25baecc36c478caf03225ef059c5e12795396025
WillLewis <willxemail@gmail.com>
2022-04-14 15:39:59 -0500

    2.8 - create a new zombie from the person by calling _createZombie with placeholder name and average of attacking zombie and the persons dna  or targetDna

--- zombieFeeding.sol
+++ zombieFeeding.sol
@@ -7,6 +7,9 @@ contract ZombieFeeding is ZombieFactory { //demonstrating inheritance
 	function feedAndMultiply(uint _zombieId, uint _targetDna) public {
 		require(msg.sender == zombieToOwner[_zombieId]); //make sure owner owns this zombie
 		Zombie storage myZombie = zombies[_zombieId]; //create a pointer to index of owners zombie
+		_targetDna = _targetDna % dnaModulus; //to only take last 16 digits
+		uint newDna = (myZombie.dna + _targetDna) / 2;
+		_createZombie("NoName", newDna);
 
 	}
 
