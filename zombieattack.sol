2d96a9b3c35f28b1cf5b367f1bb0cc369574653a
WillLewis <willxemail@gmail.com>
2022-04-27 22:21:31 -0500

    4.3 - add new file for zombieattack.sol

---
+++ zombieattack.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+
+import "./zombiehelper.sol";
+
+/**
+ * The ZombieAttack is ZombieHelper contract does this and that...
+ */
+contract ZombieAttack is ZombieHelper {
+    
+  
+}
+
