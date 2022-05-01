fd7d0981d25dd210af8f12d9d6f11ee501ce7d04
WillLewis <willxemail@gmail.com>
2022-05-01 16:47:57 -0500

    5.1 - create zombieownership.sol

---
+++ zombieownership.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.5.0 <0.6.0; //very specific about this being at top of file w/ no extra space
+
+import "./zombieattack.sol";
+
+/**
+ * The ZombieOwnership is ZombieAttack contract does this and that...
+ */
+contract ZombieOwnership is ZombieAttack {
+
+}
+
+
