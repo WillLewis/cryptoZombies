31f81acaa546f7120fadfa92db565c4f4f451016
WillLewis <willxemail@gmail.com>
2022-05-01 17:01:37 -0500

    5.2 - add erc721.sol standard contract. note its not oppenzeppelin. imported and inherited erc721 in zombieownership

--- zombieownership.sol
+++ zombieownership.sol
@@ -1,11 +1,13 @@
 pragma solidity >=0.5.0 <0.6.0; //very specific about this being at top of file w/ no extra space
 
 import "./zombieattack.sol";
+import "./erc721.sol";
+
 
 /**
  * The ZombieOwnership is ZombieAttack contract does this and that...
  */
-contract ZombieOwnership is ZombieAttack {
+contract ZombieOwnership is ZombieAttack, ERC721 {
 
 }
 
