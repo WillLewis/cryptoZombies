216e0e191f55e3c9b6a84c8a459e32bb24130ab3
WillLewis <willxemail@gmail.com>
2022-05-01 17:13:07 -0500

    5.3 - use standard erc721 function to return the number of zombies owned and the owner of any given zombie

--- zombieownership.sol
+++ zombieownership.sol
@@ -8,6 +8,23 @@ import "./erc721.sol";
  * The ZombieOwnership is ZombieAttack contract does this and that...
  */
 contract ZombieOwnership is ZombieAttack, ERC721 {
+	function balanceOf(address _owner) external view returns (uint256) {
+    // 1. Return the number of zombies `_owner` has here
+    return ownerZombieCount[_owner];
+  }
+
+  function ownerOf(uint256 _tokenId) external view returns (address) {
+    // 2. Return the owner of `_tokenId` here
+    return zombieToOwner[_tokenId];
+  }
+
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+
+  }
+
+  function approve(address _approved, uint256 _tokenId) external payable {
+
+  }
 
 }
 
