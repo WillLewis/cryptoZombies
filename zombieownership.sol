780cb0698527e3a7e16b63156a25fec91260e7f5
WillLewis <willxemail@gmail.com>
2022-05-01 22:40:04 -0500

    5.8 - add emit Approval to approve function

--- zombieownership.sol
+++ zombieownership.sol
@@ -37,7 +37,8 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
   }
 
   function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
-  	zombieApprovals[_tokenId] = _approve;	
+  	zombieApprovals[_tokenId] = _approved;	
+  	emit Approval(msg.sender, _approved, _tokenId);
 
   }
 
